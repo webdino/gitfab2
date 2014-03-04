3b5dbdeeb47a1bb04aa7278d326826961b9e88df
johnny <johnny@seurat.local>
2014-03-04 15:56:19 +0900

    initialize rails project

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+gem 'rails', '4.0.3'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+gem 'unicorn'
+gem 'capistrano', group: :development
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitfab2::Application.config.secret_key_base = '0e33e16df030b8876ac612de2f19302955bc46b1a6eeda4d74906495805a8e74b37138e21a95c5e4ad8704ad16d6560e9c96ed694c60619d33960c62a0e204ed'
