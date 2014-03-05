1b32d6710a6471258915462ae6c4a76ee794dd19
johnny <johnny@seurat.local>
2014-03-05 20:33:47 +0900

    setup capistrano and unicorn

---
+++ config/deploy/production.rb
@@ -0,0 +1,8 @@
+set :rails_env, "production"
+set :deploy_env, "production"
+set :deploy_to, "/usr/local/rails_apps/gitfab2"
+set :pid_file, "/tmp/unicorn_gitfab2.pid"
+
+role :web, "localhost"
+role :app, "localhost"
+role :db, "localhost", primary: true
