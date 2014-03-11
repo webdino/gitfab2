47f71969b81a4851f8cf5eb8233853b87c03aedb
johnny <takuma.miyake@framgia.com>
2014-03-11 06:52:31 +0000

    Task #12030 usernameの登録時にリポジトリの名前空間を作成する

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < Owner
   belongs_to :group
   has_many :contributing_recipes, through: :contributor_recipes, source: :recipe
   has_many :contributor_recipes, foreign_key: :contributor_id
+
+  after_save :prepare_namespace!
+
+  private
+  def prepare_namespace!
+    self.create_namespace!(name: self.name) unless self.namespace
+    if self.name_changed?
+      self.namespace.update_attributes name: self.name
+    end
+  end
 end
--- db/migrate/20140310060441_create_namespaces.rb
+++ db/migrate/20140310060441_create_namespaces.rb
@@ -1,7 +1,8 @@
 class CreateNamespaces < ActiveRecord::Migration
   def change
     create_table :namespaces do |t|
-      t.belongs_to :user, index: true
+      t.belongs_to :owner, index: true
+      t.string :name, index: true
 
       t.timestamps
     end
