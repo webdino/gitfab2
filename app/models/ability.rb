8ebf719a742d1999d9eb1fe51cfda07c6b0bd268
johnny <takuma.miyake@framgia.com>
2014-03-26 04:20:26 +0000

    Task #12574 リポジトリの所持者のみがリポジトリを編集できるようにする

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,7 @@ class Ability
 
   def initialize user
     user ||= User.new
+    can :manage, User, id: user.id
     can :manage, Recipe, user_id: user.id
     can :manage, Status, recipe: {user_id: user.id}
     can :manage, Material, recipe: {user_id: user.id}
