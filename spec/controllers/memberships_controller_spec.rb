fbd15f65ae35700b99aeeba4ef994d6e35bf7262
johnny <takuma.miyake@framgia.com>
2014-04-03 06:14:41 +0000

    Task #13103 fix spec

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -3,14 +3,23 @@ require "spec_helper"
 describe MembershipsController do
   disconnect_sunspot
   let(:user1){FactoryGirl.create :user}
+  let(:user2){FactoryGirl.create :user}
   let(:group1){FactoryGirl.create :group, creator: user1}
-  let(:membership1){FactoryGirl.create :membership}
-  let(:valid_attributes){{role: Membership::ROLE[:owner]}}
+  let(:membership1){group1.memberships.first}
+  let(:valid_attributes){{user_id: user2.id, group_id: group1.id, role: Membership::ROLE[:owner]}}
+
+  describe "valid attributes" do
+    subject do
+      ms = Membership.new(valid_attributes)
+      ms.valid?
+    end
+    it{should be true}
+  end
 
   describe "POST create" do
     before do
       controller.stub(:current_user).and_return user1
-      post :create, group_id: group1.id, user_id: user1.id,
+      post :create, group_id: group1.id,
         membership: membership_params, format: :json
     end
     context "with valid params" do
@@ -27,7 +36,6 @@ describe MembershipsController do
 
   describe "PATCH update" do
     before do
-      @orig_membership = membership1.dup
       controller.stub(:current_user).and_return user1
       patch :update, group_id: group1.id, id: membership1.id,
         membership: membership_params, format: :json
