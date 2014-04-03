1711f16969a43c115167caab09ffe62319182128
oshimaryo <oshimaryo@gmail.com>
2014-04-03 17:11:08 +0000

    Task#13120 headerの表示修正

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -17,12 +17,12 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    ActionController::Base.helpers.asset_path("fallback/noimage_480x360.png")
+
+    # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
