3d330c7a3d25b1f3040372452b7d87a7e1a94514
johnny <takuma.miyake@framgia.com>
2014-04-16 11:39:03 +0000

    Task #13609 Recipeの編集ページでStatusなどを追加できるようにする

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
-10.times do |t|
-  User.create(name: "user#{t}", email: "user#{t}@gitfab.org", password: "password")
+3.times do |i|
+  u = User.create(name: "user#{i}", email: "user#{i}@gitfab.org", password: "password")
+  3.times do |j|
+    u.recipes.create name: "recipe#{j}", title: "RECIPE#{j}", description: "This is RECIPE#{j}."
+  end
 end
