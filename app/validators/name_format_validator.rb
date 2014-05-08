ac4eda93b10653a56aa8c86eec0b4890e3aa4de3
johnny <takuma.miyake@framgia.com>
2014-05-08 14:14:57 +0000

    Task #14332 specs for Bug#14311

--- app/validators/name_format_validator.rb
+++ app/validators/name_format_validator.rb
@@ -1,6 +1,6 @@
 class NameFormatValidator < ActiveModel::EachValidator
   def validate_each record, attribute, value
-    unless /^[a-zA-Z]\w{3,}$/ === value
+    unless /^[a-zA-Z]\w{2,}$/ === value
       record.errors[attribute] << "is invalid format for #{attribute}."
     end
   end
