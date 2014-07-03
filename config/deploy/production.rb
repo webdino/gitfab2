4be82cf9bd996a2bc330f74e6612b39530e9081a
johnny <takuma.miyake@framgia.com>
2014-07-03 22:24:29 +0000

    remove dummy key

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,3 +3,6 @@ role :web, %w{deploy@localhost}
 role :db,  %w{deploy@localhost}
 server "localhost", user: "deploy", roles: %w{web app}
 set :rails_env, "production"
+set :default_environment, {
+  "DEVISE_SECRET_KEY" => ENV["DEVISE_SECRET_KEY"]
+}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,3 +3,6 @@ role :web, %w{deploy@localhost}
 role :db,  %w{deploy@localhost}
 server "localhost", user: "deploy", roles: %w{web app}
 set :rails_env, "staging"
+set :default_environment, {
+  "DEVISE_SECRET_KEY" => ENV["DEVISE_SECRET_KEY"]
+}
