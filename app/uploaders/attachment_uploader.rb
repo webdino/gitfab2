2e61d8f4b5443fc09b7294737800444c1526973d
dadaa <daisuke@mozilla-japan.org>
2014-07-07 22:26:25 +0900

    fixed #554 stl ファイルがアップロードできない

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -3,6 +3,8 @@
 class AttachmentUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  version :small, :if => :is_image?
+
   storage :file
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -10,6 +12,15 @@ class AttachmentUploader < CarrierWave::Uploader::Base
 
   version :small do
     process :resize_to_fit => [400, 400]
+  end
+
+  def is_image? file
+    begin
+      MiniMagick::Image.open(file.path)
+      return true
+    rescue => e
+      return false
+    end
   end
 
 end
