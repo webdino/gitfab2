f85fc0583591525c3cd32bebf0224be45a1be317
oshimaryo <oshimaryo@gmail.com>
2014-08-05 12:13:38 +0000

    close #641 recipes/show, card/edit: Figureの編集の基礎UI,UX

--- app/models/concerns/figurable.rb
+++ app/models/concerns/figurable.rb
@@ -2,6 +2,6 @@ module Figurable
   extend ActiveSupport::Concern
   included do
     embeds_many :figures, cascade_callbacks: true
-    accepts_nested_attributes_for :figures
+    accepts_nested_attributes_for :figures, allow_destroy: true
   end
 end
