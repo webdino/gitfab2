78bf1dc962e7f3c3b348a2de7bc92f4f56082dbc
dadaa <daisuke@mozilla-japan.org>
2014-08-13 21:31:16 +0900

    fixed #623 want to use a project slug in url

--- app/validators/name_format_validator.rb
+++ app/validators/name_format_validator.rb
@@ -1,7 +1,7 @@
 class NameFormatValidator < ActiveModel::EachValidator
   def validate_each record, attribute, value
-    unless /^[a-zA-Z0-9][a-zA-Z0-9\-]{2,}$/ === value
-      record.errors[attribute] << "is invalid format for #{attribute}."
+    unless /^[a-zA-Z0-9][a-zA-Z0-9\-]*$/ === value && /[^\-]$/ === value
+      record.errors[attribute] << " or permalink is invalid format."
     end
   end
 end
