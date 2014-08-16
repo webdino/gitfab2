f54e9cde3dd34365977bdfc118349054261523af
dadaa <daisuke@mozilla-japan.org>
2014-08-16 16:12:37 +0900

    fixed #720 support featured projects

--- app/controllers/global_projects_controller.rb
+++ app/controllers/global_projects_controller.rb
@@ -7,8 +7,17 @@ class GlobalProjectsController < ApplicationController
         s.fulltext q.split.map{|word| "\"#{word}\""}.join " AND "
         s.without :is_private, true
       end.results
+      @is_searching = true
     else
       @projects = Project.all().in(is_private: [false, nil]).order("updated_at DESC")
-    end      
+      @featured_projects = []
+      config = YAML.load_file("#{Rails.root}/config/featured-projects.yml")
+      if config 
+        config.each do |id|
+          @featured_projects << Project.find(id)
+        end
+      end
+      @is_searching = false
+    end
   end
 end
