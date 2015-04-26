9c2620d1c0811d33e0316a3aa4c4ca5af62e7573
Ryo Oshima <oshimaryo@gmail.com>
2015-04-26 18:30:41 +0900

    Revert "close #1060 add Card::RecipeCard class temporally"

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,6 @@ class Recipe
 
   embedded_in :project
   embeds_many :states, cascade_callbacks: true, class_name: "Card::State"
-  embeds_many :recipe_cards, cascade_callbacks: true, class_name: "Card::RecipeCard"
   accepts_nested_attributes_for :states
 
   def dup_document
