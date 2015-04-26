29983c0702de65f02058f0b2077ac09849890c83
Ryo Oshima <oshimaryo@gmail.com>
2015-04-26 18:50:33 +0900

    Revert "Revert "close #1060 add Card::RecipeCard class temporally""

---
+++ app/models/card/recipe_card.rb
@@ -0,0 +1,13 @@
+class Card::RecipeCard < Card
+  include Mongoid::Orderable
+  embedded_in :recipe
+  orderable column: :position
+
+  scope :ordered_by_position, ->{order("position ASC")}
+
+  class << self
+    def updatable_columns
+      super + [:position, :move_to]
+    end
+  end
+end
