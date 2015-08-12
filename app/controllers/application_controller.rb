aba8ea698f4f0e87c8dea5fcc7200205b488f6fa
oshimaryo <oshimaryo@gmail.com>
2015-08-12 09:48:17 +0000

    close #1184 notify 500 error in slack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   include ErrorHandling
-  handle_as_unauthorized CanCan::AccessDenied
-
   protect_from_forgery with: :exception
+
+  UnauthorizedError = Class.new(ActionController::ActionControllerError)
+  IllegalAccessError = Class.new(ActionController::ActionControllerError)
+
+  rescue_from Exception, with: :render_500
+  rescue_from ActionController::RoutingError, ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from UnauthorizedError, with: :render_401
+  rescue_from IllegalAccessError, with: :render_403
+  handle_as_unauthorized CanCan::AccessDenied
 
   before_action :verify_name
   after_filter :store_location
@@ -33,6 +40,23 @@ class ApplicationController < ActionController::Base
         !request.xhr?)
       session[:previous_url] = request.fullpath
     end
+  end
+
+  def render_401(e = nil)
+    render file: Rails.root.join("public/401.html"), status: 401, layout: false, content_type: "text/html"
+  end
+
+  def render_403(e = nil)
+    render file: Rails.root.join("public/403.html"), status: 403, layout: false, content_type: "text/html"
+  end
+
+  def render_404(e = nil)
+    render file: Rails.root.join("public/404.html"), status: 404, layout: false, content_type: "text/html"
+  end
+
+  def render_500(e = nil)
+    ExceptionNotifier.notify_exception e, env: request.env, data: {message: "500 error occured"}
+    render file: Rails.root.join("public/500.html"), status: 500, layout: false, content_type: "text/html"
   end
 
 end
