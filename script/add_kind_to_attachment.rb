192a4a8ea8f5da40929ab51bf489aab6c51f324d
oshimaryo <oshimaryo@gmail.com>
2016-02-25 06:23:12 +0000

    fix #1406 hotfix for 'script/add_kind_to_attachment.rb'

--- script/add_kind_to_attachment.rb
+++ script/add_kind_to_attachment.rb
@@ -1,7 +1,7 @@
 # use this script like
 # $bundle exec rails runner "eval(File.read 'script/add_kind_to_attachment.rb')"
 
-def change doc, class_name
+def change doc, class_name, card
   xpath = "//a[@class='#{class_name}']"
   doc.xpath(xpath).each do |dom|
     id = dom.attribute('id').value
@@ -14,10 +14,10 @@ end
 def convert card
   return if card.attachments.length == 0
   doc = Nokogiri::HTML.parse(card.description, nil, 'UTF-8')
-  change doc, 'material'
-  change doc, 'tool'
-  change doc, 'blueprint'
-  change doc, 'attachment'
+  change doc, 'material', card
+  change doc, 'tool', card
+  change doc, 'blueprint', card
+  change doc, 'attachment', card
   card.save!
 end
 
