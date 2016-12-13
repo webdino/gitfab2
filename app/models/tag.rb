dd9ce7600565789ca7fa4e6f6989b27412fe2c60
Yuichi Takeuchi <info@takeyu-web.com>
2016-12-13 17:27:26 +0900

    タグ付け時にタグ付け先の全文検索インデックスを更新

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,10 +4,18 @@ class Tag < ActiveRecord::Base
   belongs_to :user
 
   belongs_to :taggable, polymorphic: true
+  after_save :reindex
+  after_destroy :reindex
 
   class << self
     def updatable_columns
       [:_destroy, :name, :user_id]
     end
+  end
+
+  private
+  def reindex
+    taggable.solr_index if taggable.respond_to?(:solr_index)
+    true
   end
 end
