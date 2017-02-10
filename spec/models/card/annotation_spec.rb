189044ab8d701759b90fe7a25495aa6048d7803b
takeyuweb <info@takeyu-web.com>
2017-02-10 15:17:29 +0900

    ステートとアノテーションの相互変換についてのテストを追加

--- spec/models/card/annotation_spec.rb
+++ spec/models/card/annotation_spec.rb
@@ -5,14 +5,30 @@ describe Card::Annotation do
   it_behaves_like 'Orderable', :annotation
   it_behaves_like 'Orderable Scoped incrementation', [:annotation], :annotatable
 
-  let(:annotation){FactoryGirl.build :annotation}
-
   describe '.ordered_by_position' do
+    let(:annotation){ FactoryGirl.create(:annotation) }
     it { expect(Card::Annotation).to be_respond_to(:ordered_by_position) }
   end
 
   describe "#is_taggable?" do
+    let(:annotation){ FactoryGirl.create(:annotation) }
     subject{annotation.is_taggable?}
     it{should be false}
+  end
+
+  describe '#to_state!(recipe)' do
+    let!(:annotation){ FactoryGirl.create(:annotation) }
+    let!(:recipe){ FactoryGirl.create(:recipe) }
+    describe '内容を維持してStateとして作りなおす' do
+      subject!(:new_state) { annotation.to_state!(recipe) }
+      it { expect(new_state).to be_an_instance_of(Card::State) }
+      it { expect(new_state.title).to eq(annotation.title) }
+      it { expect(annotation).to be_destroyed }
+    end
+    it 'recipeのstatesに追加する' do
+      expect {
+        annotation.to_state!(recipe)
+      }.to change { recipe.states(true).count }
+    end
   end
 end
