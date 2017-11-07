aa31973efa83df9f41c8dab823dbc4328a63f177
noriyotcp <noriyo.akita@gmail.com>
2017-11-07 19:42:48 +0900

    グループ内のadminの有無によるroleの違いについてテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -90,36 +90,46 @@ describe User do
 
   describe '#is_admin_of?(group)' do
     let!(:group_administered) { FactoryGirl.create :group }
-    let!(:group_membered) { FactoryGirl.create :group }
     let!(:group_not_membered) { FactoryGirl.create :group }
-    before do
-      FactoryGirl.create(:membership, group: group_administered, user: user, role: Membership::ROLE[:admin])
-      # 先にadminとなるユーザーとgroup_memberedを紐づけておく
-      admin = FactoryGirl.create(:user)
-      FactoryGirl.create(:membership, group: group_membered, user: admin, role: Membership::ROLE[:admin])
-      # ２番目のユーザーはrole: 'editor'となる（すでにadminがグループにいるので）
-      FactoryGirl.create(:membership, group: group_membered, user: user, role: Membership::ROLE[:editor])
-    end
+    before { FactoryGirl.create(:membership, group: group_administered, user: user, role: Membership::ROLE[:admin]) }
     it { expect(user.is_admin_of?(group_administered)).to be_truthy }
-    it { expect(user.is_admin_of?(group_membered)).to be_falsey }
     it { expect(user.is_admin_of?(group_not_membered)).to be_falsey }
     it { expect(user.is_admin_of?(nil)).to be_falsey }
   end
 
   describe '#is_editor_of?(group)' do
     let!(:group_administered) { FactoryGirl.create :group }
-    let!(:group_membered) { FactoryGirl.create :group }
     let!(:group_not_membered) { FactoryGirl.create :group }
-    before do
-      FactoryGirl.create(:membership, group: group_administered, user: user, role: Membership::ROLE[:admin])
-      admin = FactoryGirl.create(:user)
-      FactoryGirl.create(:membership, group: group_membered, user: admin, role: Membership::ROLE[:admin])
-      FactoryGirl.create(:membership, group: group_membered, user: user, role: Membership::ROLE[:editor])
-    end
+    before { FactoryGirl.create(:membership, group: group_administered, user: user, role: Membership::ROLE[:admin]) }
     it { expect(user.is_editor_of?(group_administered)).to be_falsey }
-    it { expect(user.is_editor_of?(group_membered)).to be_truthy }
     it { expect(user.is_editor_of?(group_not_membered)).to be_falsey }
     it { expect(user.is_editor_of?(nil)).to be_falsey }
+  end
+
+  describe 'ユーザーをeditorとしてグループに追加するとき' do
+    let!(:group_membered) { FactoryGirl.create :group }
+
+    context 'グループにadminがすでに存在している場合' do
+      before do
+        # 先にadminとなるユーザーとgroup_memberedを紐づけておく
+        admin = FactoryGirl.create(:user)
+        FactoryGirl.create(:membership, group: group_membered, user: admin, role: Membership::ROLE[:admin])
+        # ２番目のユーザーはrole: 'editor'となる（すでにadminがグループにいるので）
+        FactoryGirl.create(:membership, group: group_membered, user: user, role: Membership::ROLE[:editor])
+      end
+      it "role: 'editor'として登録されること" do
+        expect(user.is_admin_of?(group_membered)).to be_falsey
+        expect(user.is_editor_of?(group_membered)).to be_truthy
+      end
+    end
+
+    context 'グループにadminが存在していない場合' do
+      before { FactoryGirl.create(:membership, group: group_membered, user: user, role: Membership::ROLE[:editor]) }
+      it "role: 'admin'として登録されること" do
+        expect(user.is_admin_of?(group_membered)).to be_truthy
+        expect(user.is_editor_of?(group_membered)).to be_falsey
+      end
+    end
   end
 
   describe '#membership_in(group)' do
