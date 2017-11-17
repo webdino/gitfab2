2d50a81c7e03fcef7e87d0f7f5109069c2adc373
noriyotcp <noriyo.akita@gmail.com>
2017-11-17 10:03:40 +0900

    Update framework files

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Gitfab2::Application.initialize!
+Rails.application.initialize!
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,13 @@ Rails.application.config.assets.precompile += %w(
     static_pages.js
     static_pages.css
   )
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
