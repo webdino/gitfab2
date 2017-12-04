0eac285a112550fe7843cf98d9ff3def554eeb6c
noriyotcp <noriyo.akita@gmail.com>
2017-12-04 13:15:19 +0900

    Remove useless presence: true validations
    required: true instead

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,6 +1,4 @@
 class Collaboration < ActiveRecord::Base
   belongs_to :owner, polymorphic: true, required: true
   belongs_to :project, required: true
-
-  validates :project, presence: true
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -7,7 +7,7 @@ class Membership < ActiveRecord::Base
   before_validation :must_have_user_and_group
   after_create -> { update_attributes role: ROLE[:admin] }, if: -> { group.admins.none? }
   # after_destroy -> { group.destroy }, if: -> { group && group.members.none? }
-  validates :group_id, :role, presence: :true
+  validates :role, presence: :true
 
   Membership::ROLE.keys.each do |role|
     define_method "#{role}?" do
