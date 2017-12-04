51d00f7a0f54f4eb71ce96936272360c1a2c1a47
noriyotcp <noriyo.akita@gmail.com>
2017-12-04 14:16:51 +0900

    membership.rb: Remove before_validation for user and group
    required: true instead

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,7 +4,6 @@ class Membership < ActiveRecord::Base
   belongs_to :user, required: true
   belongs_to :group, required: true
 
-  before_validation :must_have_user_and_group
   after_create -> { update_attributes role: ROLE[:admin] }, if: -> { group.admins.none? }
   # after_destroy -> { group.destroy }, if: -> { group && group.members.none? }
   validates :role, presence: :true
@@ -19,12 +18,5 @@ class Membership < ActiveRecord::Base
     def updatable_columns
       [:id, :group_id, :role, :_destroy]
     end
-  end
-
-  private
-
-  def must_have_user_and_group
-    errors.add :user, 'Error' if user.blank?
-    errors.add :group, 'Error' if group.blank?
   end
 end
