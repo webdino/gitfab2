7ae679f2e400196ee67479fbc468f46654d26dc8
noriyotcp <noriyo.akita@gmail.com>
2017-12-14 21:30:34 +0900

    Add spec for full-text search

--- spec/controllers/global_projects_controller_spec.rb
+++ spec/controllers/global_projects_controller_spec.rb
@@ -1,7 +1,8 @@
 require "spec_helper"
 
 describe GlobalProjectsController, type: :controller do
-  disconnect_sunspot
+  # テスト用にdocker-composeで建てたSolrサーバを使うのでコメントアウト
+  # disconnect_sunspot
   render_views
 
   describe 'GET index' do
@@ -23,5 +24,39 @@ describe GlobalProjectsController, type: :controller do
       before{xhr :get, :index, q: ''}
       it{expect(response).to render_template :index}
     end
+  end
+
+  shared_examples_for '検索結果' do |query, option|
+    # TODO: Solrを止めたら部分一致のテストも追加すること
+    it "完全一致(#{option})" do
+      # 公開プロジェクト
+      # デフォルトで公開になっているが明示的に
+      public_user_project = FactoryGirl.create(:user_project, :public, option)
+      public_group_prject = FactoryGirl.create(:group_project, :public, option)
+      # 非公開プロジェクト
+      private_user_project = FactoryGirl.create(:user_project, :private, option)
+      # 削除済みプロジェクト
+      deleted_user_project = FactoryGirl.create(:user_project, :soft_destroyed, option)
+      # クエリと一致しないプロジェクト
+      one_of_the_project = FactoryGirl.create(:user_project, :public)
+
+      Project.reindex
+      Sunspot.commit
+
+      get :index, q: query
+
+      aggregate_failures do
+        expect(assigns(:projects)).to include(public_user_project, public_group_prject)
+        expect(assigns(:projects)).not_to include(private_user_project)
+        expect(assigns(:projects)).not_to include(deleted_user_project)
+        expect(assigns(:projects)).not_to include(one_of_the_project)
+      end
+    end
+  end
+
+  describe '全文検索' do
+    it_behaves_like '検索結果', 'sample', name: 'sample'
+    it_behaves_like '検索結果', 'sample', title: 'sample'
+    it_behaves_like '検索結果', 'sample', description: 'sample'
   end
 end
