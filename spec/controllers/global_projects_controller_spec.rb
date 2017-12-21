7ef4440dd1420f9310ee437f454f2e289a07d9dd
noriyotcp <noriyo.akita@gmail.com>
2017-12-21 14:54:50 +0900

    検索のテストをさらに追加
    しかしprojectに関連するrecipeの検索がうまくいっていない
    recipe.statesが[]になって返ってくる？

--- spec/controllers/global_projects_controller_spec.rb
+++ spec/controllers/global_projects_controller_spec.rb
@@ -24,68 +24,284 @@ describe GlobalProjectsController, type: :controller do
     end
   end
 
-  shared_examples_for '完全一致' do |query, option|
-    it "検索結果(#{option})" do
+  shared_examples_for '検索結果' do |query|
+    it do
+
+      Project.reindex
+      Sunspot.commit
+
+      get :index, q: query
+      expect(assigns(:projects)).to include(public_user_project, public_group_project)
+
+      aggregate_failures do
+        expect(assigns(:projects)).not_to include(private_user_project)
+        expect(assigns(:projects)).not_to include(deleted_user_project)
+        expect(assigns(:projects)).not_to include(one_of_the_project)
+      end
+    end
+  end
+
+  shared_examples_for '検索結果(public user project only)' do |query|
+    it do
+
+      Project.reindex
+      Sunspot.commit
+
+      get :index, q: query
+      expect(assigns(:projects)).to include(public_user_project)
+
+      aggregate_failures do
+        expect(assigns(:projects)).not_to include(public_group_project)
+        expect(assigns(:projects)).not_to include(private_user_project)
+        expect(assigns(:projects)).not_to include(deleted_user_project)
+        expect(assigns(:projects)).not_to include(one_of_the_project)
+      end
+    end
+  end
+
+  shared_examples_for '検索結果(public group project only)' do |query|
+    it do
+
+      Project.reindex
+      Sunspot.commit
+
+      get :index, q: query
+      expect(assigns(:projects)).to include(public_group_project)
+
+      aggregate_failures do
+        expect(assigns(:projects)).not_to include(public_user_project)
+        expect(assigns(:projects)).not_to include(private_user_project)
+        expect(assigns(:projects)).not_to include(deleted_user_project)
+        expect(assigns(:projects)).not_to include(one_of_the_project)
+      end
+    end
+  end
+
+  describe 'Search projects by name' do
+    shared_context 'projects with name' do |matched, unmatched|
       # 公開プロジェクト
       # デフォルトで公開になっているが明示的に
-      public_user_project = FactoryGirl.create(:user_project, :public, option)
-      public_group_prject = FactoryGirl.create(:group_project, :public, option)
+      let!(:public_user_project) { FactoryGirl.create(:user_project, :public, name: matched) }
+      let!(:public_group_project) { FactoryGirl.create(:group_project, :public, name: matched) }
       # 非公開プロジェクト
-      private_user_project = FactoryGirl.create(:user_project, :private, option)
+      let!(:private_user_project) { FactoryGirl.create(:user_project, :private, name: matched) }
       # 削除済みプロジェクト
-      deleted_user_project = FactoryGirl.create(:user_project, :soft_destroyed, option)
-      # クエリと完全一致しないプロジェクト
-      one_of_the_project = FactoryGirl.create(:user_project, :public, title: 'samplesample')
-
-      Project.reindex
-      Sunspot.commit
-
-      get :index, q: query
-
-      expect(assigns(:projects)).to include(public_user_project, public_group_prject)
-
-      aggregate_failures do
-        expect(assigns(:projects)).not_to include(private_user_project)
-        expect(assigns(:projects)).not_to include(deleted_user_project)
-        expect(assigns(:projects)).not_to include(one_of_the_project)
-      end
+      let!(:deleted_user_project) { FactoryGirl.create(:user_project, :soft_destroyed, name: matched) }
+      # クエリと一致しないプロジェクト
+      let!(:one_of_the_project) { FactoryGirl.create(:user_project, :public, name: unmatched) }
     end
-  end
 
-  shared_examples_for '部分一致' do |query, option|
+    context '完全一致' do
+      include_context 'projects with name', 'sample', 'samplesample'
+      include_examples '検索結果', 'sample'
+    end
+
     # TODO: Solrを止めたらpendingも止めること
-    xit "検索結果(#{option})" do
-      public_user_project = FactoryGirl.create(:user_project, :public, option)
-      public_group_prject = FactoryGirl.create(:group_project, :public, option)
-      private_user_project = FactoryGirl.create(:user_project, :private, option)
-      deleted_user_project = FactoryGirl.create(:user_project, :soft_destroyed, option)
-      # クエリと部分一致しないプロジェクト
-      # TODO: もう少しパターンを増やす必要あり？
-      one_of_the_project = FactoryGirl.create(:user_project, :public, title: 'foo')
-
-      Project.reindex
-      Sunspot.commit
-
-      get :index, q: query
-
-      expect(assigns(:projects)).to include(public_user_project, public_group_prject)
-
-      aggregate_failures do
-        expect(assigns(:projects)).not_to include(private_user_project)
-        expect(assigns(:projects)).not_to include(deleted_user_project)
-        expect(assigns(:projects)).not_to include(one_of_the_project)
-      end
+    xcontext '部分一致' do
+      # TODO: name: 'foobar' のプロジェクトは返ってきて欲しい
+      include_context 'projects with name', 'foobar', 'foo'
+      include_examples '検索結果', 'foo bar'
     end
   end
 
-  describe '全文検索' do
-    it_behaves_like '完全一致', 'sample', name: 'sample'
-    it_behaves_like '完全一致', 'sample', title: 'sample'
-    it_behaves_like '完全一致', 'sample', description: 'sample'
+  describe 'Search projects by title' do
+    shared_context 'projects with title' do |matched, unmatched|
+      let!(:public_user_project) { FactoryGirl.create(:user_project, :public, title: matched) }
+      let!(:public_group_project) { FactoryGirl.create(:group_project, :public, title: matched) }
+      let!(:private_user_project) { FactoryGirl.create(:user_project, :private, title: matched) }
+      let!(:deleted_user_project) { FactoryGirl.create(:user_project, :soft_destroyed, title: matched) }
+      let!(:one_of_the_project) { FactoryGirl.create(:user_project, :public, title: unmatched) }
+    end
 
-    # クエリは空白文字区切りで単語に分ける
-    it_behaves_like '部分一致', 'foo bar', name: 'foobar'
-    it_behaves_like '部分一致', 'foo bar', title: 'foobar'
-    it_behaves_like '部分一致', 'foo bar', description: 'foobar'
+    context '完全一致' do
+      include_context 'projects with title', 'sample', 'samplesample'
+      include_examples '検索結果', 'sample'
+    end
+
+    # TODO: Solrを止めたらpendingも止めること
+    xcontext '部分一致' do
+      include_context 'projects with title', 'foobar', 'foo'
+      include_examples '検索結果', 'foo bar'
+    end
+  end
+
+  describe 'Search projects by description' do
+    shared_context 'projects with description' do |matched, unmatched|
+      let!(:public_user_project) { FactoryGirl.create(:user_project, :public, description: matched) }
+      let!(:public_group_project) { FactoryGirl.create(:group_project, :public, description: matched) }
+      let!(:private_user_project) { FactoryGirl.create(:user_project, :private, description: matched) }
+      let!(:deleted_user_project) { FactoryGirl.create(:user_project, :soft_destroyed, description: matched) }
+      let!(:one_of_the_project) { FactoryGirl.create(:user_project, :public, description: unmatched) }
+    end
+
+    context '完全一致' do
+      include_context 'projects with description', 'sample', 'samplesample'
+      include_examples '検索結果', 'sample'
+    end
+
+    # TODO: Solrを止めたらpendingも止めること
+    xcontext '部分一致' do
+      include_context 'projects with description', 'foobar', 'foo'
+      include_examples '検索結果', 'foo bar'
+    end
+  end
+
+  describe 'Search projects by tag' do
+    shared_context 'projects' do
+      let!(:public_user_project) { FactoryGirl.create(:user_project, :public) }
+      let!(:public_group_project) { FactoryGirl.create(:group_project, :public) }
+      let!(:private_user_project) { FactoryGirl.create(:user_project, :private) }
+      let!(:deleted_user_project) { FactoryGirl.create(:user_project, :soft_destroyed) }
+      let!(:one_of_the_project) { FactoryGirl.create(:user_project, :public) }
+    end
+
+    context '完全一致' do
+      let!(:tag_hash) { FactoryGirl.build(:tag, name: 'sample').attributes }
+      include_context 'projects'
+
+      before do
+        [public_user_project, public_group_project, private_user_project, deleted_user_project].each do |project|
+          project.tags.create(tag_hash)
+        end
+
+        tag_hash['name'] += '_unmatched'
+        one_of_the_project.tags.create(tag_hash)
+      end
+
+      include_examples '検索結果', 'sample'
+    end
+
+    # TODO: Solrを止めたらpendingも止めること
+    xcontext '部分一致' do
+      let!(:tag_hash) { FactoryGirl.build(:tag, name: 'foobar').attributes }
+      include_context 'projects'
+
+      before do
+        [public_user_project, public_group_project, private_user_project, deleted_user_project].each do |project|
+          project.tags.create(tag_hash)
+        end
+
+        tag_hash['name'] = 'foo'
+        one_of_the_project.tags.create(tag_hash)
+      end
+
+      include_examples '検索結果', 'foo bar'
+    end
+  end
+
+  describe 'Search projects by recipe' do
+    shared_context 'projects' do
+      let!(:public_user_project) { FactoryGirl.create(:user_project, :public) }
+      let!(:public_group_project) { FactoryGirl.create(:group_project, :public) }
+      let!(:private_user_project) { FactoryGirl.create(:user_project, :private) }
+      let!(:deleted_user_project) { FactoryGirl.create(:user_project, :soft_destroyed) }
+      let!(:one_of_the_project) { FactoryGirl.create(:user_project, :public) }
+    end
+
+    context '完全一致' do
+      include_context 'projects'
+
+      before do
+        [public_user_project, public_group_project, private_user_project, deleted_user_project].each do |project|
+          FactoryGirl.create(:state, description: 'sample', recipe: project.recipe)
+        end
+
+        FactoryGirl.create(:state, description: 'samplesample',recipe: one_of_the_project.recipe)
+      end
+
+      include_examples '検索結果', 'sample'
+    end
+
+    # TODO: Solrを止めたらpendingも止めること
+    xcontext '部分一致' do
+      include_context 'projects'
+
+      before do
+        [public_user_project, public_group_project, private_user_project, deleted_user_project].each do |project|
+          FactoryGirl.create(:state, description: 'foobar', recipe: project.recipe)
+        end
+
+        FactoryGirl.create(:state, description: 'foo',recipe: one_of_the_project.recipe)
+      end
+
+      include_examples '検索結果', 'foo bar'
+    end
+  end
+
+  shared_context 'projects with owner' do
+    let!(:public_user_project) { FactoryGirl.create(:user_project, :public, owner: user) }
+    let!(:public_group_project) { FactoryGirl.create(:group_project, :public, owner: group) }
+    let!(:private_user_project) { FactoryGirl.create(:user_project, :private, owner: user) }
+    let!(:deleted_user_project) { FactoryGirl.create(:user_project, :soft_destroyed, owner: user) }
+    let!(:one_of_the_project) { FactoryGirl.create(:user_project, :public, owner: one_of_the_users) }
+  end
+
+  describe 'Search projects by owner name' do
+    let!(:user) { FactoryGirl.create(:user, name: 'sample-user') }
+    let!(:one_of_the_users) { FactoryGirl.create(:user, name: 'one-of-the-users') }
+    let!(:group) { FactoryGirl.create(:group, name: 'sample-group') }
+
+    include_context 'projects with owner'
+
+    context '完全一致' do
+      # userとgroupで名前が重複できないのでどちらか一方だけが返ってくる結果を期待する
+      include_examples '検索結果(public user project only)', 'sample-user'
+      include_examples '検索結果(public group project only)', 'sample-group'
+    end
+
+    xcontext '部分一致' do
+      include_examples '検索結果', 'sample'
+    end
+  end
+
+  describe 'Search projects by user fullname' do
+    let!(:user) { FactoryGirl.create(:user, fullname: 'sample-user') }
+    let!(:one_of_the_users) { FactoryGirl.create(:user, fullname: 'one-of-the-users') }
+    # group has no fullnameなので今回のテスト対象ではないが
+    # shared_context内で必要なので用意する
+    let!(:group) { FactoryGirl.create(:group, name: 'sample-group') }
+
+    include_context 'projects with owner'
+
+    # groupはfullnameを持っていないので、userだけが返ってくる結果を期待する
+    context '完全一致' do
+      include_examples '検索結果(public user project only)', 'sample-user'
+    end
+
+    xcontext '部分一致' do
+      include_examples '検索結果(public user project only)', 'sample'
+    end
+  end
+
+  describe 'Search projects by url' do
+    let!(:user) { FactoryGirl.create(:user, url: 'https://sample.com') }
+    let!(:one_of_the_users) { FactoryGirl.create(:user, url: 'https://oneoftheusers.com') }
+    let!(:group) { FactoryGirl.create(:group, url: 'https://sample.com') }
+
+    include_context 'projects with owner'
+
+    context '完全一致' do
+      include_examples '検索結果', 'https://sample.com'
+    end
+
+    xcontext '部分一致' do
+      include_examples '検索結果', 'sample'
+    end
+  end
+
+  describe 'Search projects by location' do
+    let!(:user) { FactoryGirl.create(:user, location: 'Tokyo,Japan') }
+    let!(:one_of_the_users) { FactoryGirl.create(:user, location: 'Hachinohe,Japan') }
+    let!(:group) { FactoryGirl.create(:group, location: 'Tokyo,Japan') }
+
+    include_context 'projects with owner'
+
+    context '完全一致' do
+      include_examples '検索結果', 'Tokyo,Japan'
+    end
+
+    xcontext '部分一致' do
+      include_examples '検索結果', 'Tokyo Japan'
+    end
   end
 end
