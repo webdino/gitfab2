a1e4d6d6141860ae12e2c3eb924d30a8b8b26354
noriyotcp <noriyo.akita@gmail.com>
2018-01-16 17:04:46 +0900

    Use pluck(:id) on searching

--- app/controllers/global_projects_controller.rb
+++ app/controllers/global_projects_controller.rb
@@ -7,18 +7,19 @@ class GlobalProjectsController < ApplicationController
     if q.present?
       query = q.force_encoding 'utf-8'
 
-      search_by_projects = Project.published.ransack(name_or_title_or_description_cont_all: query.split).result
+      search_by_projects = Project.published.ransack(name_or_title_or_description_cont_all: query.split).result.pluck(:id)
 
-      search_by_users = Project.published.ransack(owner_of_User_type_name_or_owner_of_User_type_fullname_or_owner_of_User_type_url_or_owner_of_User_type_location_cont_all: query.split).result
+      search_by_users = Project.published.ransack(owner_of_User_type_name_or_owner_of_User_type_fullname_or_owner_of_User_type_url_or_owner_of_User_type_location_cont_all: query.split).result.pluck(:id)
 
-      search_by_groups = Project.published.ransack(owner_of_Group_type_name_or_owner_of_Group_type_url_or_owner_of_Group_type_location_cont_all: query.split).result
+      search_by_groups = Project.published.ransack(owner_of_Group_type_name_or_owner_of_Group_type_url_or_owner_of_Group_type_location_cont_all: query.split).result.pluck(:id)
 
-      search_by_tags = Project.published.ransack(tags_name_cont_all: query.split).result
+      search_by_tags = Project.published.ransack(tags_name_cont_all: query.split).result.pluck(:id)
 
-      search_by_states = Project.published.ransack(recipe_states_description_cont_all: query.split).result
-      # 検索結果から重複を取り除き、その配列をupdated_atで降順にソートする
-      results = (search_by_projects | search_by_users | search_by_groups | search_by_tags | search_by_states).sort_by(&:updated_at).reverse
-      @projects = Kaminari.paginate_array(results).page(params[:page])
+      search_by_states = Project.published.ransack(recipe_states_description_cont_all: query.split).result.pluck(:id)
+
+      ids = search_by_projects | search_by_users | search_by_groups | search_by_tags | search_by_states
+
+      @projects = Project.where(id: ids).order(updated_at: :desc).page(params[:page])
 
       @is_searching = true
       @query = query
