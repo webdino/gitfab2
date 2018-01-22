0f139d1376fb1fa82371ff40434e3c0a711c1c66
noriyotcp <noriyo.akita@gmail.com>
2018-01-22 16:23:49 +0900

    members_controller_spec: Use expect syntax instead of should
    and correct rubocop warnings

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,17 +1,19 @@
-require "spec_helper"
+# frozen_string_literal: true
+
+require 'spec_helper'
 
 describe MembersController, type: :controller do
-  let(:group){FactoryGirl.create :group}
-  let(:user){FactoryGirl.create :user}
+  let(:group) { FactoryGirl.create :group }
+  let(:user) { FactoryGirl.create :user }
 
-  subject{response}
+  subject { response }
 
-  describe "POST create" do
+  describe 'POST create' do
     before do
       xhr :post, :create, group_id: group.id, member_name: user.name
       user.reload
     end
-    it{should render_template :create}
-    it{expect(user).to have(1).memberships}
+    it { is_expected.to render_template :create }
+    it { expect(user).to have(1).memberships }
   end
 end
