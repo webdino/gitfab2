3798cc1fd1d1422de3a360260f1bf93f9588a05d
noriyotcp <noriyo.akita@gmail.com>
2018-01-24 20:14:11 +0900

    state_spec: Use expect syntax instead of should
    and correct rubocop warnings

--- spec/models/card/state_spec.rb
+++ spec/models/card/state_spec.rb
@@ -1,4 +1,6 @@
-require "spec_helper"
+# frozen_string_literal: true
+
+require 'spec_helper'
 
 describe Card::State do
   it_behaves_like 'Annotable', :state
@@ -7,21 +9,19 @@ describe Card::State do
   it_behaves_like 'Orderable Scoped incrementation', [:state], :recipe
 
   describe '.ordered_by_position' do
-
-    let(:state){FactoryGirl.build :state}
+    let(:state) { FactoryGirl.build :state }
     it { expect(Card::State).to be_respond_to(:ordered_by_position) }
   end
 
-  describe "#is_taggable?" do
-
-    let(:state){FactoryGirl.build :state}
-    subject{state.is_taggable?}
-    it{should be false}
+  describe '#is_taggable?' do
+    let(:state) { FactoryGirl.build :state }
+    subject { state.is_taggable? }
+    it { is_expected.to be false }
   end
 
   describe '#to_annotation!(parent_state)' do
-    let!(:state){ FactoryGirl.create(:state) }
-    let!(:parent_state){ FactoryGirl.create(:state) }
+    let!(:state) { FactoryGirl.create(:state) }
+    let!(:parent_state) { FactoryGirl.create(:state) }
     describe '内容を維持してAnnotationとして作りなおす' do
       subject!(:new_annotation) { state.to_annotation!(parent_state) }
       it { expect(new_annotation).to be_an_instance_of(Card::Annotation) }
@@ -29,9 +29,9 @@ describe Card::State do
       it { expect(state).to be_destroyed }
     end
     it 'parent_stateのannotationsに追加する' do
-      expect {
+      expect do
         state.to_annotation!(parent_state)
-      }.to change { parent_state.annotations(true).count }
+      end.to change { parent_state.annotations(true).count }
     end
   end
 end
