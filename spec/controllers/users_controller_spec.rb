68dbbc731dbace50eab8303dc23fc2ee905687c8
noriyotcp <noriyo.akita@gmail.com>
2018-01-24 17:04:49 +0900

    users_controller_spec: Use expect syntax instead of should
    and correct rubocop warnings

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,36 +1,38 @@
-require "spec_helper"
+# frozen_string_literal: true
+
+require 'spec_helper'
 
 describe UsersController, type: :controller do
-#  render_views
+  # render_views
 
-  subject{response}
+  subject { response }
 
-  let(:user){FactoryGirl.create :user}
+  let(:user) { FactoryGirl.create :user }
 
-  describe "GET index" do
-    before{get :index}
-    it{should render_template :index}
+  describe 'GET index' do
+    before { get :index }
+    it { is_expected.to render_template :index }
   end
 
-  describe "GET edit" do
+  describe 'GET edit' do
     before do
       get :edit, id: user
     end
-    it{should render_template :edit}
+    it { is_expected.to render_template :edit }
   end
 
-  describe "PATCH update" do
+  describe 'PATCH update' do
     before do
-      patch :update, id: user, user: {name: "foo"}
+      patch :update, id: user, user: { name: 'foo' }
       user.reload
     end
-    it{should redirect_to edit_user_path(user)}
+    it { is_expected.to redirect_to edit_user_path(user) }
   end
 
-  describe "DELETE destroy" do
+  describe 'DELETE destroy' do
     before do
       delete :destroy, id: user
     end
-    it{should redirect_to root_path}
+    it { is_expected.to redirect_to root_path }
   end
 end
