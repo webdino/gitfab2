95cab6ce6db617d11724f99d64e8ea9b7c385e53
noriyotcp <noriyo.akita@gmail.com>
2018-01-24 20:08:03 +0900

    note_card_spec: Use expect syntax instead of should
    and correct rubocop warnings

--- spec/models/card/note_card_spec.rb
+++ spec/models/card/note_card_spec.rb
@@ -1,18 +1,20 @@
-require "spec_helper"
+# frozen_string_literal: true
+
+require 'spec_helper'
 
 describe Card::NoteCard do
   it_behaves_like 'Card', :note_card
   it_behaves_like 'Taggable', :note_card
 
-  let(:project){FactoryGirl.create :user_project}
-  let(:note_card){FactoryGirl.create :note_card, note: project.note}
+  let(:project) { FactoryGirl.create :user_project }
+  let(:note_card) { FactoryGirl.create :note_card, note: project.note }
 
-  describe "#is_taggable?" do
-    subject{note_card.is_taggable?}
-    it{should be true}
+  describe '#is_taggable?' do
+    subject { note_card.is_taggable? }
+    it { is_expected.to be true }
   end
 
-  describe "#after_create" do
+  describe '#after_create' do
     it do
       expect do
         note_card.run_callbacks :create
@@ -20,11 +22,11 @@ describe Card::NoteCard do
     end
   end
 
-  describe "#after_delete" do
+  describe '#after_delete' do
     it do
       expect do
         note_card.run_callbacks :destroy
-      end.to change(note_card.note, :num_cards).by -1
+      end.to change(note_card.note, :num_cards).by(-1)
     end
   end
 end
