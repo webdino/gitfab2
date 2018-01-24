862cacae13e7e4b5f618fb0b08600f104b8db8c8
noriyotcp <noriyo.akita@gmail.com>
2018-01-24 17:14:10 +0900

    notification_spec: Use expect syntax instead of should
    and correct rubocop warnings

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,23 +1,25 @@
-require "spec_helper"
+# frozen_string_literal: true
+
+require 'spec_helper'
 
 describe Notification do
-  let(:notification){FactoryGirl.build :notification}
-  describe "#was_read_before" do
-    describe "was_read = true" do
+  let(:notification) { FactoryGirl.build :notification }
+  describe '#was_read_before' do
+    describe 'was_read = true' do
       before do
         notification.was_read = true
-        notification.updated_at = Time.zone.now - 2.hour
+        notification.updated_at = Time.zone.now - 2.hours
       end
-      subject{notification.was_read_before(1.hour)}
-      it{should be true}
+      subject { notification.was_read_before(1.hour) }
+      it { is_expected.to be true }
     end
-    describe "was_read = false" do
+    describe 'was_read = false' do
       before do
         notification.was_read = false
         notification.updated_at = Time.zone.now
       end
-      subject{notification.was_read_before(1.hour)}
-      it{should be false}
+      subject { notification.was_read_before(1.hour) }
+      it { is_expected.to be false }
     end
   end
 end
