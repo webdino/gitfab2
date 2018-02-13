0e5aeb21ee133071db3ab7cea6d2ccba076a5d5f
noriyotcp <noriyo.akita@gmail.com>
2018-02-13 13:33:48 +0900

    Remove :likes from includes
    以下の警告が出ていたので修正(見落とした？)
    
         Bullet::Notification::UnoptimizedQueryError:
           user: noriyo_tcp
    
           AVOID eager loading detected
             Project => [:likes]
             Remove from your finder: :includes => [:likes]

--- app/controllers/global_projects_controller.rb
+++ app/controllers/global_projects_controller.rb
@@ -3,7 +3,7 @@ class GlobalProjectsController < ApplicationController
 
   def index
     q = params[:q]
-    project = Project.includes(:figures, :owner, { recipe: :states }, { note: :note_cards }, :likes)
+    project = Project.includes(:figures, :owner, { recipe: :states }, { note: :note_cards })
 
     if q.present?
       query = q.force_encoding 'utf-8'
