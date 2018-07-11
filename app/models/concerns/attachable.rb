f7da113323e753108322ba1560a603bfa0d5abbe
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-07-11 15:13:33 +0900

    Remove dead codes for attachment subclasses

--- app/models/concerns/attachable.rb
+++ app/models/concerns/attachable.rb
@@ -1,13 +1,7 @@
 module Attachable
   extend ActiveSupport::Concern
   included do
-    has_many :attachments, as: :attachable, dependent: :destroy do
-      Attachment.subclasses.each do |klass|
-        define_method klass.model_name.element.pluralize do
-          where type: klass.name
-        end
-      end
-    end
+    has_many :attachments, as: :attachable, dependent: :destroy
     accepts_nested_attributes_for :attachments
   end
 end
