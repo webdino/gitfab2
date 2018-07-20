3b000690fc733b965f19ba017c39fa698ec34981
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-07-20 19:56:47 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,193 +12,193 @@
 
 ActiveRecord::Schema.define(version: 20180719073517) do
 
-  create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.string   "content"
-    t.string   "attachable_type",               null: false
-    t.integer  "attachable_id",                 null: false
-    t.string   "markup_id"
-    t.text     "link",            limit: 65535
-    t.text     "title",           limit: 65535
-    t.string   "description"
-    t.string   "kind"
-    t.string   "content_tmp"
+  create_table "attachments", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.string "content"
+    t.string "attachable_type", null: false
+    t.integer "attachable_id", null: false
+    t.string "markup_id"
+    t.text "link"
+    t.text "title"
+    t.string "description"
+    t.string "kind"
+    t.string "content_tmp"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["attachable_type", "attachable_id"], name: "index_attachments_attachable", using: :btree
+    t.index ["attachable_type", "attachable_id"], name: "index_attachments_attachable"
   end
 
-  create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.string   "title"
-    t.text     "description",   limit: 4294967295
-    t.string   "type",                                         null: false
-    t.integer  "position",                         default: 0, null: false
-    t.integer  "recipe_id"
-    t.integer  "project_id"
-    t.integer  "annotation_id"
+  create_table "cards", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.string "title"
+    t.text "description", limit: 4294967295
+    t.string "type", null: false
+    t.integer "position", default: 0, null: false
+    t.integer "recipe_id"
+    t.integer "project_id"
+    t.integer "annotation_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["annotation_id"], name: "index_cards_on_annotation_id", using: :btree
-    t.index ["project_id"], name: "index_cards_project_id", using: :btree
-    t.index ["recipe_id"], name: "index_cards_recipe_id", using: :btree
+    t.index ["annotation_id"], name: "index_cards_on_annotation_id"
+    t.index ["project_id"], name: "index_cards_project_id"
+    t.index ["recipe_id"], name: "index_cards_recipe_id"
   end
 
-  create_table "collaborations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.string   "owner_type"
-    t.integer  "owner_id"
-    t.integer  "project_id", null: false
+  create_table "collaborations", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.string "owner_type"
+    t.integer "owner_id"
+    t.integer "project_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["owner_type", "owner_id"], name: "index_collaborations_owner", using: :btree
-    t.index ["project_id"], name: "index_collaborations_project_id", using: :btree
+    t.index ["owner_type", "owner_id"], name: "index_collaborations_owner"
+    t.index ["project_id"], name: "index_collaborations_project_id"
   end
 
-  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "user_id",                  null: false
-    t.integer  "card_id",                  null: false
-    t.text     "body",       limit: 65535
+  create_table "comments", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.integer "user_id", null: false
+    t.integer "card_id", null: false
+    t.text "body"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["card_id"], name: "fk_rails_c8dff2752a", using: :btree
-    t.index ["user_id"], name: "index_comments_user_id", using: :btree
+    t.index ["card_id"], name: "fk_rails_c8dff2752a"
+    t.index ["user_id"], name: "index_comments_user_id"
   end
 
-  create_table "contributions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "contributor_id", null: false
-    t.integer  "card_id",        null: false
+  create_table "contributions", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.integer "contributor_id", null: false
+    t.integer "card_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["card_id"], name: "fk_rails_934cb2529a", using: :btree
-    t.index ["contributor_id"], name: "index_contributions_contributor_id", using: :btree
+    t.index ["card_id"], name: "fk_rails_934cb2529a"
+    t.index ["contributor_id"], name: "index_contributions_contributor_id"
   end
 
-  create_table "featured_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "feature_id"
-    t.string   "target_object_id"
-    t.string   "url"
+  create_table "featured_items", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.integer "feature_id"
+    t.string "target_object_id"
+    t.string "url"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["feature_id"], name: "index_featured_items_feature_id", using: :btree
+    t.index ["feature_id"], name: "index_featured_items_feature_id"
   end
 
-  create_table "features", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.string   "class_name"
-    t.string   "name",                   null: false
-    t.integer  "category",   default: 0, null: false
+  create_table "features", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.string "class_name"
+    t.string "name", null: false
+    t.integer "category", default: 0, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "figures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.string   "content"
-    t.string   "figurable_type"
-    t.integer  "figurable_id"
-    t.string   "link"
+  create_table "figures", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.string "content"
+    t.string "figurable_type"
+    t.integer "figurable_id"
+    t.string "link"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["figurable_type", "figurable_id"], name: "index_figures_figurable", using: :btree
+    t.index ["figurable_type", "figurable_id"], name: "index_figures_figurable"
   end
 
-  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.string   "name"
-    t.string   "avatar"
-    t.string   "slug"
-    t.string   "url"
-    t.string   "location"
-    t.integer  "projects_count", default: 0, null: false
+  create_table "groups", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.string "name"
+    t.string "avatar"
+    t.string "slug"
+    t.string "url"
+    t.string "location"
+    t.integer "projects_count", default: 0, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
-    t.index ["slug"], name: "index_users_on_slug", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
-  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "project_id", null: false
+  create_table "likes", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.integer "user_id", null: false
+    t.integer "project_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["project_id", "user_id"], name: "index_likes_unique", unique: true, using: :btree
-    t.index ["project_id"], name: "index_likes_likable", using: :btree
-    t.index ["user_id"], name: "index_likes_liker_id", using: :btree
+    t.index ["project_id", "user_id"], name: "index_likes_unique", unique: true
+    t.index ["project_id"], name: "index_likes_likable"
+    t.index ["user_id"], name: "index_likes_liker_id"
   end
 
-  create_table "memberships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "group_id"
-    t.integer  "user_id"
-    t.string   "role",       default: "editor"
+  create_table "memberships", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.integer "group_id"
+    t.integer "user_id"
+    t.string "role", default: "editor"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["group_id", "user_id"], name: "index_users_on_group_id_user_id", unique: true, using: :btree
+    t.index ["group_id", "user_id"], name: "index_users_on_group_id_user_id", unique: true
   end
 
-  create_table "notifications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "notifier_id"
-    t.integer  "notified_id"
-    t.string   "notificatable_url"
-    t.string   "notificatable_type"
-    t.string   "body"
-    t.boolean  "was_read",           default: false
+  create_table "notifications", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.integer "notifier_id"
+    t.integer "notified_id"
+    t.string "notificatable_url"
+    t.string "notificatable_type"
+    t.string "body"
+    t.boolean "was_read", default: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["notified_id"], name: "index_notifications_on_notified_id", using: :btree
-    t.index ["notifier_id"], name: "index_notifications_on_notifier_id", using: :btree
+    t.index ["notified_id"], name: "index_notifications_on_notified_id"
+    t.index ["notifier_id"], name: "index_notifications_on_notifier_id"
   end
 
-  create_table "projects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.string   "name",                                      null: false
-    t.string   "title",                                     null: false
-    t.text     "description", limit: 65535
-    t.text     "draft",       limit: 65535
-    t.boolean  "is_private",                default: false, null: false
-    t.boolean  "is_deleted",                default: false, null: false
-    t.string   "owner_type",                                null: false
-    t.integer  "owner_id",                                  null: false
-    t.string   "slug"
-    t.string   "scope"
-    t.integer  "license"
-    t.integer  "original_id"
-    t.integer  "likes_count",               default: 0,     null: false
+  create_table "projects", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.string "name", null: false
+    t.string "title", null: false
+    t.text "description"
+    t.text "draft"
+    t.boolean "is_private", default: false, null: false
+    t.boolean "is_deleted", default: false, null: false
+    t.string "owner_type", null: false
+    t.integer "owner_id", null: false
+    t.string "slug"
+    t.string "scope"
+    t.integer "license"
+    t.integer "original_id"
+    t.integer "likes_count", default: 0, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["original_id"], name: "index_projects_original_id", using: :btree
-    t.index ["owner_type", "owner_id"], name: "index_projects_owner", using: :btree
-    t.index ["slug", "owner_type", "owner_id"], name: "index_projects_slug_owner", unique: true, using: :btree
-    t.index ["updated_at"], name: "index_projects_updated_at", using: :btree
+    t.index ["original_id"], name: "index_projects_original_id"
+    t.index ["owner_type", "owner_id"], name: "index_projects_owner"
+    t.index ["slug", "owner_type", "owner_id"], name: "index_projects_slug_owner", unique: true
+    t.index ["updated_at"], name: "index_projects_updated_at"
   end
 
-  create_table "recipes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "project_id"
+  create_table "recipes", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.integer "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["project_id"], name: "index_recipes_project_id", using: :btree
+    t.index ["project_id"], name: "index_recipes_project_id"
   end
 
-  create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "user_id"
-    t.integer  "project_id"
-    t.string   "name"
+  create_table "tags", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.integer "user_id"
+    t.integer "project_id"
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["project_id"], name: "fk_rails_2f90b9163e", using: :btree
-    t.index ["user_id"], name: "index_tags_user_id", using: :btree
+    t.index ["project_id"], name: "fk_rails_2f90b9163e"
+    t.index ["user_id"], name: "index_tags_user_id"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.string   "email",          default: "", null: false
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "slug"
-    t.string   "name"
-    t.string   "fullname"
-    t.string   "avatar"
-    t.string   "url"
-    t.string   "location"
-    t.string   "authority"
-    t.integer  "projects_count", default: 0,  null: false
+  create_table "users", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+    t.string "email", default: "", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "slug"
+    t.string "name"
+    t.string "fullname"
+    t.string "avatar"
+    t.string "url"
+    t.string "location"
+    t.string "authority"
+    t.integer "projects_count", default: 0, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
-    t.index ["slug"], name: "index_users_on_slug", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   add_foreign_key "cards", "projects", name: "fk_cards_project_id"
