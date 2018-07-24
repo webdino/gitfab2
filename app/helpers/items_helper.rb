d82fbaf7ea1745f746a6cc43c0bbd596aa8f13b8
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-07-24 18:19:27 +0900

    Remove dead helper

--- app/helpers/items_helper.rb
+++
@@ -1,9 +0,0 @@
-module ItemsHelper
-  def item
-    instance_variable_get "@#{controller_name.singularize}"
-  end
-
-  def item=(val)
-    instance_variable_set "@#{controller_name.singularize}", val
-  end
-end
