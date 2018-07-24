55b9234c4ddf8b24d97694035079c9c1eef6502e
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-07-24 14:08:43 +0900

    Remove acts_as_list from Card::Usage

--- app/models/card/usage.rb
+++ app/models/card/usage.rb
@@ -27,10 +27,4 @@
 
 class Card::Usage < Card
   belongs_to :project
-  acts_as_list scope: :project
-  class << self
-    def updatable_columns
-      super + [:position]
-    end
-  end
 end
--- spec/models/card/usage_spec.rb
+++ spec/models/card/usage_spec.rb
@@ -2,8 +2,4 @@
 
 describe Card::Usage do
   it_behaves_like 'Card', :usage
-  it_behaves_like 'Orderable', :usage
-  it_behaves_like 'Orderable Scoped incrementation', [:usage], :project
-
-  let(:usage) { FactoryBot.build :usage }
 end
