acfd01893cbead2c6a0ec34c7127dfcaf723c316
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-07-24 18:35:45 +0900

    Move helper methods to controller

--- app/controllers/global_projects_controller.rb
+++ app/controllers/global_projects_controller.rb
@@ -17,16 +17,64 @@ class GlobalProjectsController < ApplicationController
 
     else
       @projects = published_projects.page(params[:page]).order(updated_at: :desc)
-      @featured_project_groups = Feature.projects.length >= 3 ? view_context.featured_project_groups : []
+      @featured_project_groups = Feature.projects.length >= 3 ? featured_project_groups : []
       @featured_groups = Group.includes(:members, :projects).order(projects_count: :desc).limit(3)
       @is_searching = false
     end
 
     selected_tags_length = 30
     @all_tags = Tag.where.not(name: '').group(:name).order(Arel.sql('COUNT(id) DESC')).pluck(:name)
-    @selected_tags = view_context.selected_tags @all_tags, selected_tags_length
+    @selected_tags = selected_tags(@all_tags, selected_tags_length)
 
-    @selected_tools = view_context.selected_tools
-    @selected_materials = view_context.selected_materials
+    @selected_tools = selected_tools
+    @selected_materials = selected_materials
   end
+
+  private
+
+    def featured_project_groups
+      project_groups = {}
+      all_featured_projects = Feature.projects
+      all_featured_projects.each do |project_group|
+        ids = project_group.featured_items.select(:target_object_id)
+        featured_projects = Project.includes(:figures, :owner, :note_cards, recipe: :states)
+                              .where(id: ids).order(likes_count: :desc)
+        project_groups[project_group.name] = featured_projects
+      end
+      project_groups.to_a
+    end
+
+    def selected_tags(list, length)
+      tags = []
+      if list.present? && list.length >= length
+        list.slice(0, length).each do |tag_name|
+          tags.push tag_name
+        end
+      end
+      tags
+    end
+
+    def selected_tools
+      tools = []
+      file_path = "#{Rails.root}/config/selected-tools.yml"
+      list = YAML.load_file file_path
+      if list.present?
+        list.each do |tool_name|
+          tools.push tool_name
+        end
+      end
+      tools
+    end
+
+    def selected_materials
+      materials = []
+      file_path = "#{Rails.root}/config/selected-materials.yml"
+      list = YAML.load_file file_path
+      if list.present?
+        list.each do |material_name|
+          materials.push material_name
+        end
+      end
+      materials
+    end
 end
