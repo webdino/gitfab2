76576a4286e51156700718497e77f444a9b07ce2
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-08-06 12:22:25 +0900

    Remove files that do not exist

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,6 @@ Rails.application.config.assets.precompile += %w(
     application.js
     groups.js    groups.css
     dashboard.js dashboard.css
-    posts.js     posts.css
     project.css  project.js
     user.js      user.css
     global_projects.js
