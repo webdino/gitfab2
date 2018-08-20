75ed0ab7d81a616c0e84d38e91ba2c293370cd75
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-08-20 18:50:40 +0900

    Refactoring

--- app/controllers/project_comments_controller.rb
+++ app/controllers/project_comments_controller.rb
@@ -2,13 +2,12 @@ class ProjectCommentsController < ApplicationController
   def create
     project = Project.find(params[:project_id])
     project_comment = project.project_comments.build(project_comment_params)
-    project_comment.user = current_user
 
     if project_comment.save
       notify_users(project)
-      redirect_to project_path(project.owner.slug, project, anchor: "project-comment-#{project_comment.id}")
+      redirect_to project_path(project.owner, project, anchor: "project-comment-#{project_comment.id}")
     else
-      redirect_to project_path(project.owner.slug, project, anchor: "project-comment-form"),
+      redirect_to project_path(project.owner, project, anchor: "project-comment-form"),
                   alert: project_comment.errors.full_messages,
                   flash: { project_comment_body: project_comment.body }
     end
@@ -19,15 +18,15 @@ class ProjectCommentsController < ApplicationController
     project = project_comment.project
 
     unless project_comment.manageable_by?(current_user)
-      redirect_to project_path(project.owner.slug, project, anchor: "project-comments"),
+      redirect_to project_path(project.owner, project, anchor: "project-comments"),
                   alert: 'You can not delete a comment'
       return
     end
 
     if project_comment.destroy
-      redirect_to project_path(project.owner.slug, project, anchor: "project-comments")
+      redirect_to project_path(project.owner, project, anchor: "project-comments")
     else
-      redirect_to project_path(project.owner.slug, project, anchor: "project-comments"),
+      redirect_to project_path(project.owner, project, anchor: "project-comments"),
                   alert: 'Comment could not be deleted'
     end
   end
@@ -35,15 +34,14 @@ class ProjectCommentsController < ApplicationController
   private
 
     def project_comment_params
-      params.require(:project_comment).permit(:body)
+      params.require(:project_comment).permit(:body).merge(user: current_user)
     end
 
     def notify_users(project)
       users = project.notifiable_users(current_user)
       return if users.blank?
 
-      url = project_path(project, owner_name: project.owner.slug)
       body = "#{current_user.name} commented on #{project.title}."
-      project.notify(users, current_user, url, body)
+      project.notify(users, current_user, project_path(project.owner, project), body)
     end
 end
