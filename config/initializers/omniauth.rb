480572143e6597933d0e7489783e86fa36e347ff
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-09-21 19:59:02 +0900

    Add Google login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     scope: "read:user,user:email",
     callback_path: "/users/auth/github/callback"
   }
+  provider :google_oauth2, ENV["GOOGLE_APP_ID"], ENV["GOOGLE_APP_SECRET"], {
+    callback_path: "/users/auth/google_oauth2/callback"
+  }
   provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], {
     callback_path: "/users/auth/facebook/callback"
   }
