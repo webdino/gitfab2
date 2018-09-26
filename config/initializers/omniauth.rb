64ab7b7490ac53d20a39ed1ac7e5243804ed11d9
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-09-26 13:04:32 +0900

    Precise scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET'], { callback_path: "/users/auth/github/callback" }
+  provider :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET'], {
+    scope: "read:user,user:email",
+    callback_path: "/users/auth/github/callback"
+  }
 end
