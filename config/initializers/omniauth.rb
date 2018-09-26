a1790bcf0a4904b9ccf5d989b932d1079605d1eb
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-09-26 13:11:20 +0900

    Replace single quotes to double quotes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET'], {
+  provider :github, ENV["GITHUB_APP_ID"], ENV["GITHUB_APP_SECRET"], {
     scope: "read:user,user:email",
     callback_path: "/users/auth/github/callback"
   }
