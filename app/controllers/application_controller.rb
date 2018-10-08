a57a8bd47a6ce2d4cc91062fd928a3c6940ebb79
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-10-08 21:24:18 +0900

    Fix calling handlers
    引数がある場合はSymbol渡しできない

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   unless Rails.env.development?
-    rescue_from StandardError, with: :render_500
-    rescue_from ActiveRecord::RecordNotFound, with: :render_404
-    rescue_from ActionController::RoutingError, with: :render_404
+    rescue_from StandardError, with: -> { render_500 }
+    rescue_from ActiveRecord::RecordNotFound, with: -> { render_404 }
+    rescue_from ActionController::RoutingError, with: -> { render_404 }
   end
-  rescue_from CanCan::AccessDenied, with: :render_401
+  rescue_from CanCan::AccessDenied, with: -> { render_401 }
 
   after_action :store_location
 
