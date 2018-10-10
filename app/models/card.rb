7d915cf4aef1951e14d3a86d3f00b3b7aa7db7a8
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-10-10 22:39:21 +0900

    Workaround bugfix for ActiveRecord::SubclassNotFound

--- app/models/card.rb
+++ app/models/card.rb
@@ -64,4 +64,33 @@ class Card < ApplicationRecord
       true
     end
   end
+
+
+  # Override ActiveRecord::Inheritance::ClassMethods#find_sti_class
+  #
+  # バグ対策のオーバーライド。
+  # 原因が突き止められなかったので、場当たり策ではあるが、Railsのメソッドをオーバーライドする。
+  # 原因を突き止めて直すか、もしくは、もっと根本的なところでCardのSTIをやめたい。
+  #
+  # 問題が発生するようになった原因コミットはこれ。
+  # https://github.com/rails/rails/commit/7c0f8c64fa1e8e055e2077255843f149e600024b
+  # ただ、このコミット自体は間違っていなくて、元からあった問題がこのコミットで顕在化したと思われる。
+  #
+  # Card::State.createなどSTIの子クラスを作る際、create後にbelongs_to先であるProjectがCardを取得し直すらしく（この認識が正しいかは怪しい）、
+  # その取得し直しはSTIを考慮していないみたいで他のSTIなクラスも一緒に取得して最終的にこの`find_sti_class`に関係のないtype_nameを渡し、
+  # その結果、"Card::Usage is not a subclass of Card::State"のエラーが発生する。
+  # そのため、そのパターンのエラーのみ無視するようにしている。
+  def self.find_sti_class(type_name)
+    super
+  rescue ActiveRecord::SubclassNotFound => e
+    if e.message.include?("Invalid single-table inheritance type")
+      if store_full_sti_class
+        ActiveSupport::Dependencies.constantize(type_name)
+      else
+        compute_type(type_name)
+      end
+    else
+      raise e
+    end
+  end
 end
