3a187b2fb8ae926fed70c14c794cb8bcb570a02f
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2018-10-24 14:22:13 +0900

    Use https in development mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "localhost", protocol: "https", port: 3000 }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
