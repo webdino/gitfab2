2d6a9d037def685ad1f0d147831b7942aeff2fa5
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2020-09-17 01:19:26 +0900

    Add permission check for member management
    グループへのメンバーの追加におけるspecを追加とセキュリティ修正

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,13 +1,18 @@
 class MembersController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
-    user = User.friendly.find(params[:member_name])
-    membership = user.join_to(@group)
-    if membership
-      @member = membership.user
-      membership.role = params[:role]
-      membership.save
-      render :create
+
+    if can?(:manage, @group)
+      user = User.friendly.find(params[:member_name])
+      membership = user.join_to(@group)
+      if membership
+        @member = membership.user
+        membership.role = params[:role]
+        membership.save
+        render :create
+      else
+        render json: { success: false }
+      end
     else
       render json: { success: false }
     end
--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,19 +1,50 @@
 # frozen_string_literal: true
 
 describe MembersController, type: :controller do
-  let(:group) { FactoryBot.create :group }
-  let(:user) { FactoryBot.create :user }
-
-  subject { response }
-
   describe 'POST create' do
-    before do
-      post :create, params: { group_id: group.id, member_name: user.name }, xhr: true
-      user.reload
+    let(:group) { FactoryBot.create :group }
+    let(:user) { FactoryBot.create :user }
+
+    subject { post :create, params: { group_id: group.id, member_name: user.name }, xhr: true }
+
+    context 'when current_user is the group administrator' do
+      before do
+        sign_in FactoryBot.create(:membership, group: group, role: 'admin').user
+      end
+      
+      it { is_expected.to render_template :create }
+      it 'creates a membership' do
+        expect { subject }.to change(user.memberships, :count).to eq 1
+      end
     end
-    it { is_expected.to render_template :create }
-    it 'has 1 membership' do
-      expect(user.memberships.size).to eq 1
+
+    context 'when current_user is the group editor' do
+      before do
+        sign_in FactoryBot.create(:membership, group: group, role: 'editor').user
+      end
+      
+      it { is_expected.to_not render_template :create }
+      it 'does not create a membership' do
+        expect { subject }.to_not change(user.memberships, :count)
+      end
+    end
+
+    context 'when current_user is not the group member' do
+      before do
+        sign_in FactoryBot.create(:user)
+      end
+      
+      it { is_expected.to_not render_template :create }
+      it 'does not create a membership' do
+        expect { subject }.to_not change(user.memberships, :count)
+      end
+    end
+
+    context 'when current_user is an anonymous' do
+      it { is_expected.to_not render_template :create }
+      it 'does not create a membership' do
+        expect { subject }.to_not change(user.memberships, :count)
+      end
     end
   end
 end
