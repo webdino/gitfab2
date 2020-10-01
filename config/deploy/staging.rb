35cdaf2d2d1a2b3ee98b28cd479bbdffbe5d9e7e
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2020-10-01 16:40:20 +0900

    Update CRONTAB during deployment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 role :app, %w(deploy@localhost)
 role :web, %w(deploy@localhost)
 role :db,  %w(deploy@localhost)
-server 'localhost', user: 'deploy', roles: %w(web app)
+server 'localhost', user: 'deploy', roles: %w(web app cron)
 set :rails_env, 'staging'
 set :linked_files, %w{config/database.yml config/secrets.yml}
